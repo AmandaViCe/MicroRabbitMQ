9d29d5754a33e440eca1c3edda44e14bf1da590d
developer20 <developer20@JNM076>
2024-09-26 09:56:41 -0300

    Adicionar arquivos de projeto.

---
+++ ProducerConsumerRabbitMQ/Consumer/Receiver.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    public class Receiver
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "moura",
+                Password = "Mour@505050"
+            };
+            using ( var connection = factory.CreateConnection()) 
+            using ( var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "Chat", 
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel); 
+                    consumer.Received += (model, ea) =>  // delegates
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("Mensagem recebida {0} ... ", message);
+                    };
+
+                    channel.BasicConsume(queue: "Chat", 
+                                         autoAck: true, // Aviso que foi recebida a msg
+                                         consumer: consumer);
+
+                Console.WriteLine("Aperte [enter] para sair... ");
+                Console.ReadLine();
+                }
+
+        }
+    }
+}
---
+++ ProducerConsumerRabbitMQ/Producer/Sender.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading.Channels;
+
+
+var factory = new ConnectionFactory() //Criação de uma factory
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "moura",
+    Password = "Mour@505050"
+}; 
+
+using (var connection = factory.CreateConnection())  // Abriu uma conexão
+using (var channel = connection.CreateModel()) // Abriu um canal
+{
+    channel.QueueDeclare(queue: "Chat",        // Declarou a fila     
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    Console.WriteLine("Digite sua mensagem e aperte <ENTER>"); // Criou uma menssagem
+
+    var messages = new List<string>();
+
+    while (true)
+    {
+        string message = Console.ReadLine();
+
+        if (message == "")
+            break;
+
+        messages.Add(message);
+
+    }
+
+    foreach (var msg in messages)
+    {
+
+    
+        var body = Encoding.UTF8.GetBytes(s:msg);
+
+        channel.BasicPublish(exchange: string.Empty,
+                                       routingKey: "Chat",
+                                       basicProperties: null,
+                                       body: body);
+
+        Console.WriteLine($"[x] Enviando {msg}");
+
+        Console.WriteLine("Aperte [enter] para sair...");
+        Console.ReadLine();
+    }
+}
+    
+
